0ee217500945f95520a4fa8e22c8fa35615872b7
johnjibin <jibin.johnxr@gmail.com>
2022-09-03 22:21:26 +0530

    First HTML Page

--- First.js
+++ First.js
@@ -1,6 +1,16 @@
 var http = require('http');
-
+var fs = require('fs');
 http.createServer(function(req,res){
-	res.writeHead(200,{'Content-Type':'text/plain'});
-	res.end('Hello World!');
+	res.writeHead(200,{'Content-Type':'text/html'});
+	fs.readFile('index.html',null,function(error,data){
+		if (error) {
+			res.writeHead(404);
+			res.write("File not Found");
+		}
+		else{
+			res.write(data);
+		}
+	})
+	//res.end('Hello World!');
+	res.end();
 }).listen(8080);
